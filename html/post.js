888217dea7a582b081292fcc12e1d754808670c5
Smbo-2002 <smbo.voskanyan2002@gmail.com>
2016-08-03 16:23:06 +0400

    to

--- html/post.js
+++ html/post.js
@@ -1,51 +1,70 @@
-(function() {
-    
+(function () {
+    'use strict';
 	class blogFormController {
 
-		 constructor ($http) {
+		 constructor (Blog) {
 			this.posts = [];
 
-			this.http = $http;
+			this.blog = Blog;
 
-			$http.get('/api')
-			.then((data) => {
-				this.posts = data.data;
+		  Blog.query()
+			.$promise.then((data) => {
+				this.posts = data;
 			})
-			.catch((err) => {	
-				alert("something gone wrong");
-			});
 		}
 
-		savePost (data){
-			this.http.post('/api/', data)
-			.then((data) => {
-				this.posts.push(data.data);
+		save (data){
+			this.blog.create(data)
+			.$promise.then((data) => {
+				this.posts.push(data);
 				this.data.text = "";
 				console.log(this.posts);
 			})
 			.catch(err => alert("something gone wrong"))
 		}
 
-		delPost (index){
-			this.http.delete('/api/'+ this.posts[index]._id)
-			.then(() => {
+		del (index){
+			this.blog.delete({id: this.posts[index]._id})
+			.$promise.then(() => {
 				this.posts.splice(index, 1);
 			})
 		}
 
-		edPost (index){
-			this.http.put('/api/'+ this.posts[index]._id, this.posts[index])
-			.then((data) => {
-				this.posts[index] = data.data;
+		ed (index){
+			console.log({id: this.posts[index]._id});
+			this.blog.edit({id: this.posts[index]._id}, this.posts[index])
+			.$promise.then((data) => {
+				this.posts[index] = data;
 			})
 		}
 	}
 	// let blogFormController = $injector.instantiate(blogFormController)
 	var app = angular.module('postApp', ['ngClassified','ngResource'])
 	.constant('$classify', true)
-	.controller('blogFormController', blogFormController);
+	.controller('blogFormController', blogFormController)
+    .factory('Blog', ($resource => {
+        var blogApiUrl = '/api';
+        return $resource(blogApiUrl,{},
+            	{
+                query: {
+                    method: 'GET',
+                    url: blogApiUrl,
+                    isArray: true
+                },
+            
+                create: {method: 'POST'},
+            
+                edit: {
+                    method: 'PUT',
+                    url: blogApiUrl + '/:id'
+                },
 
-	 app.factory('Blog', $resource => {
-	 	return $resource('/api/');
-	 });
+                delete: {
+                    method: 'DELETE',
+                    url: blogApiUrl + '/:id'
+                }
+			
+            });
+
+    }));
 })();
